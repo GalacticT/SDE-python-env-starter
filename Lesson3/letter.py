293488eca0bf0b77a5868bafd698cbbc7aaa11be
GalacticT <tommyiseasyas123asd@gmail.com>
2025-06-05 17:26:56 -0500

    Create letter.py

---
+++ Lesson3/letter.py
@@ -0,0 +1,4 @@
+def thank_you(name):
+    print(f"Dear {name},\n\nThank you so much for your gift for Christmas/my graduation/my birthday/etc. It means so much to me!\n\nYours Truly,\n(Your name)")
+
+thank_you("Alice")
