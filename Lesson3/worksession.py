0f1260870d27f7524cd61d92e3f506878251c89e
GalacticT <tommyiseasyas123asd@gmail.com>
2025-06-05 17:52:48 -0500

    Create worksession.py

---
+++ Lesson3/worksession.py
@@ -0,0 +1,6 @@
+numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+for number in numbers:
+    for suit in suits:
+        print(number, "of", suit)
