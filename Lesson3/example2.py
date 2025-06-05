40e7896377deaa09d06aca5f0c219d208169c3f0
GalacticT <tommyiseasyas123asd@gmail.com>
2025-06-05 17:22:10 -0500

    Update example2.py

--- Lesson3/example2.py
+++ Lesson3/example2.py
@@ -6,3 +6,9 @@ def say_hello(name):
 
 # Our function is defined above, so now we can 'call' it
 say_hello('Bob')
+# Add a second parameter called ‘City’
+# Update the print statement so it says: “Hello            , welcome to                 .”
+# Call the function using a name and a city to see if it works!
+def say_hello(name, city):
+    print(f'Hello {name}, welcome to {city}!')
+say_hello('Bob', 'Garland')
