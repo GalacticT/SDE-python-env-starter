4a9def5753e64318d70ee42a1120ab8482df2fdd
GalacticT <tommyiseasyas123asd@gmail.com>
2025-06-17 17:07:04 -0500

    Create top_netflix_shows.py

---
+++ top_netflix_shows.py
@@ -0,0 +1,2 @@
+top_netflix_shows = ['Stranger Things', 'All American: Homecoming', 'Jurassic World Camp Cretaceous', 'The Umbrella Academy', 'Blown Away', 'Money Heist']
+print(top_netflix_shows[3])
